24e8467d9d0b5f3d74ac2f2d6652af5ae62ea39e
Siluxsept <ronny.horn.rh@gmail.com>
2021-11-19 21:03:39 +0100

    mt6797-common:: update dependencies and kernel toolchain version
    Change-Id: Icebf1d68eeba683ae5d1a6ce4123554e03175879

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,7 +28,7 @@ BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE = bootopt=64S3,32N2,64N2
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04f88000 --second_offset=0x00e88000 --tags_offset 0x03f88000
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-gnu-6.3.1/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-gnu-6.4.1/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_NEEDS_DTBOIMAGE := true
 TARGET_DTBO_IMAGE_NAME := odmdtbo
